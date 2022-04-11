f5ca3d286fe4f138d60db05c0b93d9646b9a7aaf
Kenneth Dow <kdow@epages.com>
2022-04-11 23:34:22 +0200

    Clean up Macros

--- lib/macros/src/macros.h
+++ lib/macros/src/macros.h
@@ -7,15 +7,12 @@
 //
 #define SECOND_IN_US 1000000
 #define DEFAULT_BEATS_PER_MINUTE 120
-#define DEFAULT_SEQUENCE_LENGTH 16
-
 
 //
 //VOICE
 //
 
 #define PULSES_PER_QUARTER_NOTE 4
-#define TOTAL_NUMBER_OF_VOICES  4
 
 //ALIASES FOR VOICES
 
@@ -34,13 +31,8 @@
 //
 //WIRELESS SERVICES
 //
-#define DEFAULT_AP_SSID         "sequenceAll"
-#define DEFAULT_AP_PASSWORD     "transLiberationNow"
 
 #define LOCAL_UDP_PORT                   8000
 #define DEFAULT_REMOTE_UDP_PORT          57120
-#define DEFAULT_REMOTE_IP                "192.168.4.2"
-
-
 
 #endif
