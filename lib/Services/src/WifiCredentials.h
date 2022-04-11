cb7d7831a7f15d19bf4fc3dda241c6621771bca4
Kenneth Dow <kdow@epages.com>
2022-04-11 23:52:13 +0200

    Remove unused struct

--- lib/Services/src/WifiCredentials.h
+++ lib/Services/src/WifiCredentials.h
@@ -7,23 +7,10 @@
 #ifndef SEQUENCEALL_WIFICREDENTIALS_H
 #define SEQUENCEALL_WIFICREDENTIALS_H
 
-typedef struct WiFiCredentials {
-    std::string uri;
-    std::string ssid;
-    std::string pwd;
-
-    WiFiCredentials(std::string uri,
-                    std::string ssid,
-                    std::string pwd)
-            : uri(std::move(uri)), ssid(std::move(ssid)), pwd(std::move(pwd)) {}
-
-    WiFiCredentials() = default;
-} WiFiCredentials;
-
 typedef struct WiFiCredentialsChar {
-    char uri[32];
-    char ssid[32];
-    char pwd[32];
+    char uri[32]{};
+    char ssid[32]{};
+    char pwd[32]{};
 
     WiFiCredentialsChar(const char *Uri,
                     const char *Ssid,
